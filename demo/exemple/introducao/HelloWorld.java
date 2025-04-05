29cd4b1607c0ba0e150e5f9ce9b50c262c5e15f5
João Ricardo <joao.luiz.ric@gmail.com>
2025-04-05 16:39:38 -0300

    Initial commit course Java

---
+++ demo/exemple/introducao/HelloWorld.java
@@ -0,0 +1,10 @@
+package demo.exemple.introducao;
+public class HelloWorld {
+    /**
+     * Isso é um comentário <b>javadoc</b>
+     * @param args
+    */
+    public static void main(String[] args) {
+        System.out.println("Olá");
+    }
+}
