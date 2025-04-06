5aed41b342f07c78800b2a6ce560a5a6324477c8
Joao Ricardo <joao.luiz.ric@gmail.com>
2025-04-05 22:28:05 -0300

    New lessons

--- app/src/demo/exemple/coursejava/introducao/Aula02TiposPrimitivos.java
+++ app/src/demo/exemple/coursejava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,20 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int age = 19;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean varTrue = true;
+        boolean varFalse = false;
+        char character = 'M';
         System.out.println("A idade é " + age + " anos");
+        System.out.println(varTrue);
+        System.out.println(varFalse);
+        System.out.println(character);
+        System.out.println(ageByte);
+        System.out.println(ageShort);
+        System.out.println(salaryDouble);
+        System.out.println(salaryFloat);
     }
 }
